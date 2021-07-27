2f94853c17532037a6f2d04fdee8b9a8514f7dfe
Java Online Projects <admin@javaops.ru>
2021-07-27 17:17:16 +0600

    BootJava. Lesson 5. Patch 3. Account hateoas paging.

--- src/main/java/ru/javaops/bootjava/error/IllegalRequestDataException.java
+++ src/main/java/ru/javaops/bootjava/error/IllegalRequestDataException.java
@@ -3,10 +3,10 @@ package ru.javaops.bootjava.error;
 import org.springframework.boot.web.error.ErrorAttributeOptions;
 import org.springframework.http.HttpStatus;
 
-import static org.springframework.boot.web.error.ErrorAttributeOptions.Include.MESSAGE;
+import static org.springframework.boot.web.error.ErrorAttributeOptions.Include.*;
 
 public class IllegalRequestDataException extends AppException {
     public IllegalRequestDataException(String msg) {
-        super(HttpStatus.UNPROCESSABLE_ENTITY, msg, ErrorAttributeOptions.of(MESSAGE));
+        super(HttpStatus.UNPROCESSABLE_ENTITY, msg, ErrorAttributeOptions.of(MESSAGE, STACK_TRACE, EXCEPTION));
     }
 }
